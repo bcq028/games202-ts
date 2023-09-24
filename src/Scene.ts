4f8836d1c5643246536eacb4cbe187190cea54ed
bcq028 <108849949+bcq028@users.noreply.github.com>
2023-09-24 18:52:06 +0800

    clear scene

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,14 +1,44 @@
-import { Camera } from "three";
+import * as THREE from 'three'
 import { Mesh } from "./Mesh";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-export class Scene{
-    public meshes:Mesh[]=[]
-    public lights:Mesh[]=[]
-    public camera:Camera
-    public addMesh(mesh:Mesh){
+const cameraPosition = [-20, 180, 250];
+
+export class Scene {
+    public meshes: Mesh[] = []
+    public lights: Mesh[] = []
+    public camera: THREE.Camera
+    public cameraControls:OrbitControls
+    constructor(canvas:HTMLCanvasElement) {
+        const { camera, cameraControls } = this.camera_init(canvas)
+        this.camera = camera;
+        this.cameraControls=cameraControls
+    }
+    public addMesh(mesh: Mesh) {
         this.meshes.push(mesh)
     }
-    public addLight(light:Mesh){
+    public addLight(light: Mesh) {
         this.lights.push(light)
+    }
+
+    public camera_init(canvas: HTMLCanvasElement) {
+        const camera = new THREE.PerspectiveCamera(75, (canvas as HTMLCanvasElement).clientWidth / (canvas as HTMLCanvasElement).clientHeight);
+        camera.position.set(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+
+        const cameraControls = new OrbitControls(camera, canvas);
+        cameraControls.enableZoom = true;
+        cameraControls.enableRotate = true;
+        cameraControls.enablePan = true;
+        cameraControls.rotateSpeed = 0.3;
+        cameraControls.zoomSpeed = 1.0;
+        cameraControls.panSpeed = 2.0;
+        cameraControls.target.set(0, 1, 0);
+        const setSize = (width: number, height: number) => {
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+        }
+        setSize(canvas.clientWidth, canvas.clientHeight);
+        window.addEventListener('resize', () => setSize(canvas.clientWidth, canvas.clientHeight));
+        return { camera, cameraControls };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 import { EmissiveMaterial } from './Material';
-import {  cube } from './Geometry';
+import { cube } from './Geometry';
 import { Scene } from './Scene';
 import { RenderPipeline } from './RenderPipeline';
 import { loadOBJ } from './loader';
-const cameraPosition = [-20, 180, 250];
 
 
 function main() {
@@ -14,40 +11,15 @@ function main() {
   canvas.width = window.screen.width;
   canvas.height = window.screen.height;
   const gl = canvas.getContext('webgl')!;
-
-  const camera_init = () => {
-    const camera = new THREE.PerspectiveCamera(75, (gl.canvas as HTMLCanvasElement).clientWidth / (gl.canvas as HTMLCanvasElement).clientHeight);
-    camera.position.set(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
-
-    const cameraControls = new OrbitControls(camera, canvas);
-    cameraControls.enableZoom = true;
-    cameraControls.enableRotate = true;
-    cameraControls.enablePan = true;
-    cameraControls.rotateSpeed = 0.3;
-    cameraControls.zoomSpeed = 1.0;
-    cameraControls.panSpeed = 2.0;
-    cameraControls.target.set(0, 1, 0);
-    const setSize = (width: number, height: number) => {
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-    }
-    setSize(canvas.clientWidth, canvas.clientHeight);
-    window.addEventListener('resize', () => setSize(canvas.clientWidth, canvas.clientHeight));
-    return { camera, cameraControls };
-  }
-
-  const { camera, cameraControls } = camera_init()
-
-
   const pointLight = {
     geometry: cube(),
     material: new EmissiveMaterial(250, [1, 1, 1])
   };
-  const scene = new Scene();
-  scene.camera = camera;
+
+  const scene = new Scene(canvas);
   scene.addLight(pointLight);
   loadOBJ(scene, 'assets/mary/', 'Marry');
-  
+
   const renderPipeline = new RenderPipeline()
 
   const guiParams = {
@@ -77,14 +49,11 @@ function main() {
   createGUI();
 
   function mainLoop() {
-    cameraControls.update();
-
+    scene.cameraControls.update();
     renderPipeline.render(guiParams, gl, scene);
     requestAnimationFrame(mainLoop);
   }
   requestAnimationFrame(mainLoop);
-
-
 }
 
 main();
