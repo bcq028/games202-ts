e1db320f61b12669ecb28608849c0ea13e5ff7d6
bcq028 <108849949+bcq028@users.noreply.github.com>
2023-09-26 12:58:13 +0800

    todo:understand matrixworld and apply viewmatrix

--- src/Material.ts
+++ src/Material.ts
@@ -1,68 +1,67 @@
 import { PhongVertexShader, PhongFragmentShader, LightCubeVertexShader, LightCubeFragmentShader } from './loader';
 
 export interface ShaderVar {
-    type: string,
-    value: any
+  type: string,
+  value: any
 }
 
 export class Material {
-    uniform_keys: string[] = []
-    attibute_keys: string[] = []
-    uniforms: { [key: string]: ShaderVar }
-    attribs: string[]
-    vsSrc: string = ""
-    fsSrc: string = ""
-    constructor(uniforms: { [key: string]: ShaderVar }, attribs: string[], vsSrc: string, fsSrc: string) {
-        this.uniform_keys = ['uModelViewMatrix', 'uProjectionMatrix', 'uCameraPos', 'uLightPos'];
-        this.uniform_keys.push(...Object.keys(uniforms));
-        this.attibute_keys.push(...Object.keys(attribs));
-        this.uniforms = uniforms;
-        this.attribs = attribs;
-        this.fsSrc = fsSrc;
-        this.vsSrc = vsSrc;
-    }
-    setMeshAttribs(attribs: string[]) {
-        this.attibute_keys.push(...attribs)
-    }
+  uniform_keys: string[] = []
+  attibute_keys: string[] = []
+  uniforms: { [key: string]: ShaderVar }
+  attribs: string[]
+  vsSrc: string = ""
+  fsSrc: string = ""
+  constructor(uniforms: { [key: string]: ShaderVar }, attribs: string[], vsSrc: string, fsSrc: string) {
+    this.uniform_keys = ['uModelMatrix', 'uViewMatrix', 'uProjectionMatrix', 'uCameraPos', 'uLightPos'];
+    this.uniform_keys.push(...Object.keys(uniforms));
+    this.attibute_keys.push(...Object.keys(attribs));
+    this.uniforms = uniforms;
+    this.attribs = attribs;
+    this.fsSrc = fsSrc;
+    this.vsSrc = vsSrc;
+  }
+  setMeshAttribs(attribs: string[]) {
+    this.attibute_keys.push(...attribs)
+  }
 }
 
 export class PhongMaterial extends Material {
-    constructor(color: number[], colorMap: HTMLImageElement, specular: number[], intensity: number) {
-      let textureSample = 0;
-  
-      if (colorMap != null) {
-        textureSample = 1;
-        super({
-          'uTextureSample': { type: '1i', value: textureSample },
-          'uSampler': { type: 'texture', value: colorMap },
-          'uKd': { type: '3fv', value: color },
-          'uKs': { type: '3fv', value: specular },
-          'uLightIntensity': { type: '1f', value: intensity }
-        }, [], PhongVertexShader, PhongFragmentShader);
-      } else {
-        //console.log(color);
-        super({
-          'uTextureSample': { type: '1i', value: textureSample },
-          'uKd': { type: '3fv', value: color },
-          'uKs': { type: '3fv', value: specular },
-          'uLightIntensity': { type: '1f', value: intensity }
-        }, [], PhongVertexShader, PhongFragmentShader);
-      }
-  
-    }
-  }
-  
-  export class EmissiveMaterial extends Material {
-    intensity: number
-    color: number[]
-    constructor(lightIntensity: number, lightColor: number[]) {
+  constructor(color: number[], colorMap: HTMLImageElement, specular: number[], intensity: number) {
+    let textureSample = 0;
+
+    if (colorMap != null) {
+      textureSample = 1;
       super({
-        'uLigIntensity': { type: '1f', value: lightIntensity },
-        'uLightColor': { type: '3fv', value: lightColor }
-      }, [], LightCubeVertexShader, LightCubeFragmentShader);
-  
-      this.intensity = lightIntensity;
-      this.color = lightColor;
+        'uTextureSample': { type: '1i', value: textureSample },
+        'uSampler': { type: 'texture', value: colorMap },
+        'uKd': { type: '3fv', value: color },
+        'uKs': { type: '3fv', value: specular },
+        'uLightIntensity': { type: '1f', value: intensity }
+      }, [], PhongVertexShader, PhongFragmentShader);
+    } else {
+      //console.log(color);
+      super({
+        'uTextureSample': { type: '1i', value: textureSample },
+        'uKd': { type: '3fv', value: color },
+        'uKs': { type: '3fv', value: specular },
+        'uLightIntensity': { type: '1f', value: intensity }
+      }, [], PhongVertexShader, PhongFragmentShader);
     }
+
   }
-  
+}
+
+export class EmissiveMaterial extends Material {
+  intensity: number
+  color: number[]
+  constructor(lightIntensity: number, lightColor: number[]) {
+    super({
+      'uLigIntensity': { type: '1f', value: lightIntensity },
+      'uLightColor': { type: '3fv', value: lightColor }
+    }, [], LightCubeVertexShader, LightCubeFragmentShader);
+
+    this.intensity = lightIntensity;
+    this.color = lightColor;
+  }
+}
--- src/RenderPass.ts
+++ src/RenderPass.ts
@@ -27,8 +27,8 @@ export function reset_gl(gl: WebGLRenderingContext) {
 
 
 export class CameraRenderPass {
-    uniforms: Record<string, WebGLUniformLocation> = {}
-    attribs: Record<string, number> = {}
+    uniformLocation: Record<string, WebGLUniformLocation> = {}
+    attributeLayout: Record<string, number> = {}
     constructor(private gl: WebGLRenderingContext) {
 
     }
@@ -36,14 +36,14 @@ export class CameraRenderPass {
     setShaderLocations(shaderProgram: WebGLProgram, uniforms: string[], attribs: string[]) {
         if (uniforms.length) {
             for (let i = 0; i < uniforms.length; ++i) {
-                Object.assign(this.uniforms, {
+                Object.assign(this.uniformLocation, {
                     [uniforms[i]]: this.gl.getUniformLocation(shaderProgram, uniforms[i]),
                 });
             }
         }
         if (attribs.length) {
             for (let i = 0; i < attribs.length; ++i) {
-                Object.assign(this.attribs, {
+                Object.assign(this.attributeLayout, {
                     [attribs[i]]: this.gl.getAttribLocation(shaderProgram, attribs[i]),
                 });
             }
@@ -52,29 +52,30 @@ export class CameraRenderPass {
 
     draw_forward(scene: Scene, guiParams: GUIParams, lightPos: [number, number, number]) {
 
-        let modelViewMatrix = mat4.create();
+        let ModelMatrix = mat4.create();
+        let ViewMatrix = mat4.create();
         let projectionMatrix = mat4.create();
         const modelTranslation = [guiParams.modelTransX, guiParams.modelTransY, guiParams.modelTransZ] as [number, number, number];
         const modelScale = [guiParams.modelScaleX, guiParams.modelScaleY, guiParams.modelScaleZ] as [number, number, number];
         let transform = new TRSTransform(modelTranslation, modelScale);
-        mat4.invert(modelViewMatrix, scene.camera.matrixWorld.elements as unknown as [
+        mat4.invert(ViewMatrix, scene.camera.matrixWorld.elements as [
             number, number, number, number,
             number, number, number, number,
             number, number, number, number,
             number, number, number, number
         ]);
-        let dynamicLightMatrix = structuredClone(modelViewMatrix);
+        let dynamicLightMatrix = structuredClone(ModelMatrix);
         mat4.translate(dynamicLightMatrix, dynamicLightMatrix, lightPos);
 
-        mat4.translate(modelViewMatrix, modelViewMatrix, transform.translate);
-        mat4.scale(modelViewMatrix, modelViewMatrix, transform.scale);
+        mat4.translate(ModelMatrix, ModelMatrix, transform.translate);
+        mat4.scale(ModelMatrix, ModelMatrix, transform.scale);
 
-        let scaledMatrix = structuredClone(modelViewMatrix);
+        let scaledMatrix = structuredClone(ModelMatrix);
         let t = structuredClone(transform.scale);
         for (let i = 0; i < t.length; ++i) {
             t[i] = 1 / t[i];
         }
-        mat4.scale(modelViewMatrix, scaledMatrix, t);
+        mat4.scale(ModelMatrix, scaledMatrix, t);
         mat4.copy(projectionMatrix, scene.camera.projectionMatrix.elements as unknown as [
             number, number, number, number,
             number, number, number, number,
@@ -85,37 +86,37 @@ export class CameraRenderPass {
         scene.camera.updateMatrixWorld();
         for (let [material, meshes] of scene.rhiBatchedEntities) {
             if ('intensity' in scene.RhiMaterial2Material.get(material)) {
-                modelViewMatrix = dynamicLightMatrix;
+                ModelMatrix = dynamicLightMatrix;
             } else {
-                modelViewMatrix = scaledMatrix;
+                ModelMatrix = scaledMatrix;
             }
             this.setShaderLocations(material.shaderProgram, scene.RhiMaterial2Material.get(material).uniform_keys, scene.RhiMaterial2Material.get(material).attibute_keys);
             this.gl.useProgram(material.shaderProgram);
             if (lightPos) {
-                this.gl.uniform3fv(this.uniforms.uLightPos, lightPos);
+                this.gl.uniform3fv(this.uniformLocation.uLightPos, lightPos);
             }
             for (let k in scene.RhiMaterial2Material.get(material).uniforms) {
                 if (scene.RhiMaterial2Material.get(material).uniforms[k].type == 'matrix4fv') {
                     this.gl.uniformMatrix4fv(
-                        this.uniforms[k],
+                        this.uniformLocation[k],
                         false,
                         scene.RhiMaterial2Material.get(material).uniforms[k].value);
                 } else if (scene.RhiMaterial2Material.get(material).uniforms[k].type == '3fv') {
                     this.gl.uniform3fv(
-                        this.uniforms[k],
+                        this.uniformLocation[k],
                         scene.RhiMaterial2Material.get(material).uniforms[k].value);
                 } else if (scene.RhiMaterial2Material.get(material).uniforms[k].type == '1f') {
                     this.gl.uniform1f(
-                        this.uniforms[k],
+                        this.uniformLocation[k],
                         scene.RhiMaterial2Material.get(material).uniforms[k].value);
                 } else if (scene.RhiMaterial2Material.get(material).uniforms[k].type == '1i') {
                     this.gl.uniform1i(
-                        this.uniforms[k],
+                        this.uniformLocation[k],
                         scene.RhiMaterial2Material.get(material).uniforms[k].value);
                 } else if (scene.RhiMaterial2Material.get(material).uniforms[k].type == 'texture') {
                     this.gl.activeTexture(this.gl.TEXTURE0);
                     this.gl.bindTexture(this.gl.TEXTURE_2D, createTexture(this.gl, scene.RhiMaterial2Material.get(material).uniforms[k].value));
-                    this.gl.uniform1i(this.uniforms[k], 0);
+                    this.gl.uniform1i(this.uniformLocation[k], 0);
                 }
             }
             for (let mesh of meshes) {
@@ -127,14 +128,14 @@ export class CameraRenderPass {
                     const offset = 0;
                     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, mesh.vertexBuffer);
                     this.gl.vertexAttribPointer(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).verticesName],
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).verticesName],
                         numComponents,
                         type,
                         normalize,
                         stride,
                         offset);
                     this.gl.enableVertexAttribArray(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).verticesName]);
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).verticesName]);
                 }
                 if (mesh.normalBuffer) {
                     const numComponents = 3;
@@ -144,14 +145,14 @@ export class CameraRenderPass {
                     const offset = 0;
                     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, mesh.normalBuffer);
                     this.gl.vertexAttribPointer(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).normalsName],
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).normalsName],
                         numComponents,
                         type,
                         normalize,
                         stride,
                         offset);
                     this.gl.enableVertexAttribArray(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).normalsName]);
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).normalsName]);
                 }
                 if (mesh.texcoordBuffer) {
                     const numComponents = 2;
@@ -161,27 +162,30 @@ export class CameraRenderPass {
                     const offset = 0;
                     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, mesh.texcoordBuffer);
                     this.gl.vertexAttribPointer(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).texcoordsName],
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).texcoordsName],
                         numComponents,
                         type,
                         normalize,
                         stride,
                         offset);
                     this.gl.enableVertexAttribArray(
-                        this.attribs[scene.RhiMesh2Mesh.get(mesh).texcoordsName]);
+                        this.attributeLayout[scene.RhiMesh2Mesh.get(mesh).texcoordsName]);
                 }
                 this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, mesh.indicesBuffer);
                 this.gl.uniformMatrix4fv(
-                    this.uniforms.uProjectionMatrix,
+                    this.uniformLocation.uProjectionMatrix,
                     false,
                     projectionMatrix);
                 this.gl.uniformMatrix4fv(
-                    this.uniforms.uModelViewMatrix,
+                    this.uniformLocation.uModelMatrix,
                     false,
-                    modelViewMatrix);
-
+                    ModelMatrix);
+                this.gl.uniformMatrix4fv(
+                    this.uniformLocation.uViewMatrix,
+                    false,
+                    ViewMatrix);
                 this.gl.uniform3fv(
-                    this.uniforms.uCameraPos,
+                    this.uniformLocation.uCameraPos,
                     [scene.camera.position.x, scene.camera.position.y, scene.camera.position.z]);
                 const vertexCount = scene.RhiMesh2Mesh.get(mesh).count;
                 const type = this.gl.UNSIGNED_SHORT;
